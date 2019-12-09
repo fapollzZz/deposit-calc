b50f30ebc81964490ff150464967a560e4833f2d
fap <kmilvat@github.com>
2019-12-09 16:44:08 +0700

    Vvod data

--- ez.cpp
+++ ez.cpp
@@ -7,5 +7,16 @@ using namespace std;
 
 int main() {
 
-	cout << "Hello World";
+	int x, y, z, a;
+	cout << "¬ведите срок:=" << endl;
+	cin >> x;
+	cout << "¬ведите сумму вклада:= " << endl;
+	cin >> z;
+
+	if ((x >= 0) & (x <= 30))
+	{
+		y = -10;
+		a = z / 100 * y + z;
+		cout << "—умма вклада =: " << a;
+	}
 }
