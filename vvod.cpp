200e5ec4addb13bae964ac903dae61b430cbe017
fap <kmilvat@github.com>
2019-12-09 16:42:13 +0700

    Vvod data

---
+++ vvod.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x, y, z, a;
+	cout << "Введите срок:=" << endl;
+	cin >> x;
+	cout << "Введите сумму вклада:= " << endl;
+	cin >> z;
+
+	if ((x >= 0) & (x <= 30))
+	{
+		y = -10;
+		a = z / 100 * y + z;
+		cout << "Сумма вклада =: " << a;
+	}
+}
